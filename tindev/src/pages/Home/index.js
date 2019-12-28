16f1d773e892f7cbdca42ee1f8506b76dd5071a5
Danilo Marques <danilomarques20@hotmail.com>
2019-12-28 18:46:39 -0300

    Adicionando conteudo no Home

--- tindev/src/pages/Home/index.js
+++ tindev/src/pages/Home/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
-
+import Logo from '../../assets/logo.png';
 import {
     Container,
-    Title
+    ImageLogo,
+    Avatar,
+    CardContainer,
+    Card,
+    Footer,
+    DevName,
+    DevBio
 } from './styles';
 
 export default function Home() {
     return (
         <Container > 
-            <Title>Ola, Home!</Title>
+            <ImageLogo  source={Logo}/>
+            <CardContainer>
+                <Card>
+                    <Avatar source={{uri: 'https://avatars1.githubusercontent.com/u/72895?v=4'}} />
+                    <Footer>
+                        <DevName>
+                            Danilo Marques
+                        </DevName>
+                        <DevBio numberOfLines={3}>We will win self driving cars. We will win self driving cars. We will win self driving cars. We will win self driving cars. We will win self driving cars. </DevBio>
+                    </Footer>
+                </Card>
+            </CardContainer>
         </Container>
     )
 }
